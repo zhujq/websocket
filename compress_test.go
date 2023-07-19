7c8458e3370be1b4d64b44b096ff9c71ad5c012f
zhujq <14065567@qq.com>
2023-07-19 11:17:12 +0800

    Update compress_test.go

--- compress_test.go
+++ compress_test.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"testing"
 
-	"nhooyr.io/websocket/internal/test/assert"
-	"nhooyr.io/websocket/internal/test/xrand"
+	"github.com/zhujq/websocket/internal/test/assert"
+	"github.com/zhujq/websocket/internal/test/xrand"
 )
 
 func Test_slidingWindow(t *testing.T) {
