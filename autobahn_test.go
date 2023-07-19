5cb34b8d5c9ed0d12a64e9159ad7fdbb8ffee9f9
zhujq <14065567@qq.com>
2023-07-19 11:16:21 +0800

    Update autobahn_test.go

--- autobahn_test.go
+++ autobahn_test.go
@@ -15,10 +15,10 @@ import (
 	"testing"
 	"time"
 
-	"nhooyr.io/websocket"
-	"nhooyr.io/websocket/internal/errd"
-	"nhooyr.io/websocket/internal/test/assert"
-	"nhooyr.io/websocket/internal/test/wstest"
+	"github.com/zhujq/websocket"
+	"github.com/zhujq/websocket/internal/errd"
+	"github.com/zhujq/websocket/internal/test/assert"
+	"github.com/zhujq/websocket/internal/test/wstest"
 )
 
 var excludedAutobahnCases = []string{
